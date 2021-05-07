f4192dc317f3f3def1a1b96b1805987c1bad4815
Nick Porter (in lab) <jacioneportier@gmail.com>
2021-05-06 18:38:10 -0600

    Needs further debugging.
    The script can now read the image data correctly...sort of. It places pixels in the correct place in the image, but usually doesn't finish reading all the pixels. You end up with half an image, padded with zeros where it didn't finish reading.
    
    Sometimes it also fails and returns a NULL pointer (see SDK p. 20)

--- experiment/mightex.py
+++ experiment/mightex.py
@@ -15,6 +15,9 @@ class Camera:
         filedir = 'C:/Users/jacione/Documents/Mightex_SM_software/SDK/Lib/x64'
         self.dll = CDLL(f'{filedir}/SSClassic_USBCamera_SDK.dll')
 
+        self.is_on = False
+        self.dtype = 'i2'
+
         # Basic IO functions for connecting/disconnecting with the camera
         self.__sdk_InitDevice = self.dll.SSClassicUSB_InitDevice
         self.__sdk_InitDevice.argtypes = []
@@ -55,7 +58,7 @@ class Camera:
         self.__sdk_SetResolution.restype = c_int
         self.width = resolution[0]
         self.height = resolution[1]
-        self.resolution = resolution
+        self.im_shape = (self.height, self.width)
         self.im_size = self.width * self.height
 
         self.__sdk_SetExposure = self.dll.SSClassicUSB_SetExposureTime
@@ -74,9 +77,9 @@ class Camera:
         self.__sdk_InstallFrameHooker.restype = c_int
 
         self.__sdk_GetCurrentFrame = self.dll.SSClassicUSB_GetCurrentFrame16bit
-        self.__sdk_GetCurrentFrame.argtypes = [c_int, c_int, np.ctypeslib.ndpointer('i2', 2, resolution)]
+        self.__sdk_GetCurrentFrame.argtypes = [c_int, c_int, np.ctypeslib.ndpointer(self.dtype, 1, (self.im_size+127,))]
                                             # [type,    ID,     pointer to data]
-        self.__sdk_GetCurrentFrame.restype = np.ctypeslib.ndpointer('i2', 1, self.im_size)
+        self.__sdk_GetCurrentFrame.restype = np.ctypeslib.ndpointer(self.dtype, 1, (self.im_size+127,))
 
         # Initialize the camera
         num_cameras = self.__sdk_InitDevice()
@@ -86,8 +89,6 @@ class Camera:
         add = self.__sdk_AddCamera(1)
         if add == -1:
             raise IOError('Could not add camera device to working set!')
-
-        self.is_on = False
 
         self.camera_on()
         self.set_defaults()
@@ -101,11 +102,13 @@ class Camera:
         if self.is_on:
             self.camera_off()
         self.__sdk_UnInitDevice()
+        print('SUCCESS: Camera disconnected!')
 
     def __del__(self):
         if self.is_on:
             self.camera_off()
-        self.__sdk_UnInitDevice()
+            self.__sdk_UnInitDevice()
+            print('SUCCESS: Camera disconnected!')
 
     def camera_on(self):
         if self.is_on:
@@ -152,6 +155,7 @@ class Camera:
             self.width = width
             self.height = height
             self.im_size = self.width * self.height
+            self.im_shape = (self.height, self.width)
         return
 
     def set_exposure(self, ms):
@@ -187,13 +191,14 @@ class Camera:
     def get_frame(self):
         if not self.is_on:
             print('Camera is not initialized!')
-        sptr = np.empty(self.resolution, dtype='i2')
+        sptr = np.empty(self.im_size+127, dtype=self.dtype)
         self.__sdk_InstallFrameHooker(1, None)
         data = self.__sdk_GetCurrentFrame(0, 1, sptr)
+        # FIXME: For some reason this doesn't always read the whole image data.
 
-        data = data.reshape(self.resolution).T
-        print(np.min(data), np.max(data))
-        plt.imshow(data)
+        data = data[127:].reshape(self.im_shape)
+        plt.imshow(data, clim=[80, 125])
+        plt.tight_layout()
         plt.show()
         return data
 
@@ -201,4 +206,3 @@ class Camera:
 if __name__ == '__main__':
     with Camera() as cam:
         cam.get_frame()
-        cam.camera_off()
